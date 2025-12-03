3d2b6667c4806809d19e5c83f1c9e8f1e0660536
Daniel Dias <danieldias@gruporaval.com.br>
2025-12-03 15:00:50 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,30 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(-1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(extType)) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      }
+    }
   },
   server: {
     port: 3000,
     open: true
+  },
+  css: {
+    devSourcemap: false
   }
 })
